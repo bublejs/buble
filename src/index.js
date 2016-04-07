3af7e2401a7ca082cfc91791982fb2e1c439ecd3
Rich Harris <richard.a.harris@gmail.com>
2016-04-07 18:20:16 -0400

    use sourceType: module

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import Program from './program/Program.js';
 export function transform ( source, options ) {
 	var ast = parse( source, {
 		ecmaVersion: 6,
-		preserveParens: true
+		preserveParens: true,
+		sourceType: 'module'
 	});
 
 	var program = new Program( source, ast );
