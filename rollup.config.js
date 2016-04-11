291c393cc1f0e1ac21aea21672af81a4c2cfd6ea
Rich Harris <richard.a.harris@gmail.com>
2016-04-11 12:57:11 -0400

    add browser build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+var external = process.env.DEPS ? null : [ 'acorn', 'magic-string' ];
 
 export default {
 	entry: 'src/index.js',
 	moduleName: 'buble',
-	plugins: [ buble() ],
+	plugins: [
+		buble({
+			include: [ 'src/**', 'node_modules/acorn/**' ]
+		})
+	],
 	sourceMap: true
 };
