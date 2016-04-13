55c042c436f1952cacba1d6df1f7cfba61791d1e
Rich Harris <richard.a.harris@gmail.com>
2016-04-13 17:10:05 -0400

    fix bugs introduced with #12 fix

--- src/program/types/ReturnStatement.js
+++ src/program/types/ReturnStatement.js
@@ -3,15 +3,22 @@ import Node from '../Node.js';
 export default class ReturnStatement extends Node {
 	initialise () {
 		this.loop = this.findNearest( /(?:For|While)Statement/ );
-		if ( this.loop ) this.loop.canReturn = true;
+		this.nearestFunction = this.findNearest( /Function/ );
+
+		if ( this.loop && ( !this.nearestFunction || this.loop.depth > this.nearestFunction.depth ) ) {
+			this.loop.canReturn = true;
+			this.shouldWrap = true;
+		}
+
+		this.argument.initialise();
 	}
 
 	transpile ( code ) {
-		if ( this.loop && this.loop.shouldRewriteAsFunction ) {
-			code.insert( this.argument.start, `{ v: ` );
-			code.insert( this.argument.end, ` }` );
-		}
+		const shouldWrap = this.shouldWrap && this.loop && this.loop.shouldRewriteAsFunction;
+		if ( shouldWrap ) code.insert( this.argument.start, `{ v: ` );
 
-		super.transpile( code );
+		this.argument.transpile( code );
+
+		if ( shouldWrap ) code.insert( this.argument.end, ` }` );
 	}
 }
