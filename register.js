5bc24f0db6430c1be05578297e11ce9428e7dfa5
Rich Harris <richard.a.harris@gmail.com>
2016-06-22 11:14:58 -0400

    support node 6 in buble/register

--- register.js
+++ register.js
@@ -6,11 +6,11 @@ var original = require.extensions[ '.js' ];
 var nodeModulesPattern = path.sep === '/' ? /\/node_modules\// : /\\node_modules\\/;
 
 var nodeVersion = /(?:0\.)?\d+/.exec( process.version )[0];
-var versions = [ '0.10', '0.12', '4', '5' ];
+var versions = [ '0.10', '0.12', '4', '5', '6' ];
 
 if ( !~versions.indexOf( nodeVersion ) ) {
-	if ( +nodeVersion > 5 ) {
-		nodeVersion = 5;
+	if ( +nodeVersion > 6 ) {
+		nodeVersion = 6;
 	} else {
 		throw new Error( 'Unsupported version (' + nodeVersion + '). Please raise an issue at https://gitlab.com/Rich-Harris/buble/issues' );
 	}
