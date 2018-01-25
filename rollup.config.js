9ab1c9c7dc87fc18f609394942240a12b432c686
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-01-25 23:46:41 +0100

    Add browser builds targeting es5 envs

--- rollup.config.js
+++ rollup.config.js
@@ -4,48 +4,62 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
-const config = {
-	input: 'src/index.js',
-	plugins: [
-		json(),
-		commonjs(),
-		buble({
-			target: {
-				node: 4
-			},
-			include: [
-				'src/**',
-				'node_modules/acorn-object-spread/**',
-				'node_modules/unicode-loose-match/**',
-				'node_modules/regexpu-core/**'
-			],
-			transforms: {
-				dangerousForOf: true
-			}
-		}),
-		resolve()
-	]
+const ensureArray = maybeArr => Array.isArray(maybeArr) ? maybeArr : [maybeArr];
+
+const createConfig = (opts) => {
+	opts = opts || {};
+	const browser = opts.browser || false;
+	const external = opts.external || ['acorn', 'magic-string'];
+	const output = ensureArray(opts.output);
+
+	return {
+		input: 'src/index.js',
+		output: output.map(format => Object.assign({}, format, {
+			name: 'buble',
+			sourcemap: true
+		})),
+		external: external,
+		plugins: [
+			json(),
+			commonjs(),
+			buble({
+				target: !browser ? { node: 4 } : null,
+				include: [
+					'src/**',
+					'node_modules/unicode-loose-match/**',
+					'node_modules/regexpu-core/**'
+				],
+				transforms: {
+					dangerousForOf: true
+				}
+			}),
+			resolve()
+		],
+	};
 };
 
-const globals = {
-  'acorn/dist/acorn.js': 'acorn',
-  'magic-string': 'MagicString'
-};
-
-export default [
-	/* ESM/UMD builds */
-	Object.assign({}, config, {
-		external: ['acorn/dist/acorn.js', 'magic-string'],
+const configs = [
+	/* node ESM/CJS builds */
+	createConfig({
 		output: [
-			{ format: 'es', file: pkg.module, globals, sourcemap: true, name: 'buble' },
-			{ format: 'umd', file: pkg.main, globals, sourcemap: true, name: 'buble' }
-		]
+			{ format: 'es', file: pkg.module },
+			{ format: 'cjs', file: pkg.main }
+		],
 	}),
-
-	/* UMD with bundled dependencies, for browsers */
-	Object.assign({}, config, {
+	/* browser ESM/CJS builds (for bundlers) */
+	createConfig({
+		browser: true,
 		output: [
-			{ format: 'umd', file: pkg.browser, sourcemap: true, name: 'buble' }
-		]
-	})
+			{ format: 'es', file: pkg.browser[pkg.module] },
+			{ format: 'cjs', file: pkg.browser[pkg.main] }
+		],
+	}),
+	/* UMD with bundled dependencies, ready for browsers */
+	createConfig({
+		browser: true,
+		external: [],
+		output: { format: 'umd', file: pkg.unpkg },
+	}),
 ];
+
+export default configs;
