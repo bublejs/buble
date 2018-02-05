bublejs/buble

commit 16675bb146b9584e4c37e264068f44d3bbab3abb

Correctly handle nbsp (closes #46)

src/program/types/JSXElement.js
@@ -21,7 +21,7 @@ export default class JSXElement extends Node {
 			if (child.type !== 'Literal') return true;
 
 			// remove whitespace-only literals, unless on a single line
-			return /\S/.test(child.value) || !/\n/.test(child.value);
+			return /\S/.test(child.raw) || !/\n/.test(child.raw);
 		});
 
 		if (children.length) {
@@ -43,7 +43,7 @@ export default class JSXElement extends Node {
 				}
 
 				if (child.type === 'Literal') {
-					const str = normalise(child.value, i === children.length - 1);
+					const str = normalise(child.raw, i === children.length - 1);
 					code.overwrite(child.start, child.end, str);
 				}
 

test/samples/jsx.js
@@ -272,5 +272,20 @@ module.exports = [
 		output: `
 			React.createElement( Thing, { 'data-foo': true })
 		`
+	},
+
+	{
+		description: 'handles non-breaking white-space entities',
+
+		input: `
+			<div>
+				<a>1</a>&nbsp;
+				&nbsp;
+			</div>
+		`,
+		output: `
+			React.createElement( 'div', null,
+				React.createElement( 'a', null, "1" ), "&nbsp; &nbsp;")
+		`
 	}
 ];

commit 569dd0746d540d4ae7d2225345a921c47a1bf3fb

Restore converting HTML entities in JSX
I broke this in 16675bb146b9584e4c37e264068f44d3bbab3abb. Closes #104.

src/program/types/JSXElement.js
@@ -1,6 +1,9 @@
 import Node from '../Node.js';
 
 function normalise(str, removeTrailingWhitespace) {
+
+	str = str.replace(/\u00a0/g, '&nbsp;');
+
 	if (removeTrailingWhitespace && /\n/.test(str)) {
 		str = str.replace(/\s+$/, '');
 	}
@@ -43,7 +46,7 @@ export default class JSXElement extends Node {
 				}
 
 				if (child.type === 'JSXText') {
-					const str = normalise(child.raw, i === children.length - 1);
+					const str = normalise(child.value, i === children.length - 1);
 					code.overwrite(child.start, child.end, str);
 				}
 

test/samples/jsx.js
@@ -293,5 +293,19 @@ module.exports = [
 			React.createElement( 'div', null,
 				React.createElement( 'a', null, "1" ), "&nbsp; &nbsp;")
 		`
+	},
+
+	{
+		description: 'transpiles entities',
+
+		input: `
+			<div>
+				<a>1&lt;</a>&nbsp;
+			</div>
+		`,
+		output: `
+			React.createElement( 'div', null,
+				React.createElement( 'a', null, "1<" ), "&nbsp;")
+		`
 	}
 ];