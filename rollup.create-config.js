461d7cffb98512c89e5ddfa746b5dc4478f6ab2b
Adrian Heine <mail@adrianheine.de>
2018-10-07 20:58:22 +0200

    Transpile acorn-jsx, too

--- rollup.create-config.js
+++ rollup.create-config.js
@@ -25,6 +25,7 @@ const createConfig = (opts) => {
 				target: !browser ? { node: 4 } : null,
 				include: [
 					'src/**',
+					'node_modules/acorn-jsx/**',
 					'node_modules/regexpu-core/**',
 					'node_modules/unicode-match-property-ecmascript/**',
 					'node_modules/unicode-match-property-value-ecmascript/**',
