bublejs/buble

commit 15befd8ad9d60525ceeef19df79b1a58a06da5f3

Bundle acorn dependencies since acorn-jsx is not node 4 compatible

rollup.create-config.js
@@ -9,7 +9,7 @@ const ensureArray = maybeArr => Array.isArray(maybeArr) ? maybeArr : [maybeArr];
 const createConfig = (opts) => {
 	opts = opts || {};
 	const browser = opts.browser || false;
-	const external = opts.external || Object.keys(pkg.dependencies || {});
+	const external = opts.external || Object.keys(pkg.dependencies || {}).filter(dep => !dep.match(/^acorn/));
 	const output = ensureArray(opts.output);
 
 	return {
@@ -26,10 +26,7 @@ const createConfig = (opts) => {
 				target: !browser ? { node: 4 } : null,
 				include: [
 					'src/**',
-					'node_modules/acorn-jsx/**',
-					'node_modules/regexpu-core/**',
-					'node_modules/unicode-match-property-ecmascript/**',
-					'node_modules/unicode-match-property-value-ecmascript/**',
+					'node_modules/acorn-jsx/**'
 				],
 				transforms: {
 					dangerousForOf: true

commit 3a1e5d158d371362681e5765f224d5331fb9b2d2

Fix unpkg build
I broke this in 15befd8ad9d60525ceeef19df79b1a58a06da5f3.

rollup.create-config.js
@@ -26,7 +26,11 @@ const createConfig = (opts) => {
 				target: !browser ? { node: 4 } : null,
 				include: [
 					'src/**',
-					'node_modules/acorn-jsx/**'
+					'node_modules/acorn-jsx/**',
+					// For unpkg build
+					'node_modules/regexpu-core/**',
+					'node_modules/unicode-match-property-ecmascript/**',
+					'node_modules/unicode-match-property-value-ecmascript/**',
 				],
 				transforms: {
 					dangerousForOf: true