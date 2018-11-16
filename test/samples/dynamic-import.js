77cf57d743077108587f091d5f24487359fd252f
Adrian Heine <mail@adrianheine.de>
2018-11-16 14:18:17 +0100

    Fix dynamic-import test

--- test/samples/dynamic-import.js
+++ test/samples/dynamic-import.js
@@ -1,6 +1,7 @@
 module.exports = [
 	{
 		description: 'support dynamic import',
+		options: { transforms: { moduleImport: false } },
 		input: `import('./module.js')`,
 		output: `import('./module.js')`
 	}
