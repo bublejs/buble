789d175fc839814f590d99a5adc34a7bda5e756c
Curran <curran.kelleher@gmail.com>
2021-07-13 09:31:00 -0400

    Remove more files

--- src/program/types/index.js
+++ src/program/types/index.js
@@ -2,10 +2,6 @@ import ArrowFunctionExpression from './ArrowFunctionExpression.js';
 import FunctionExpression from './FunctionExpression.js';
 import Identifier from './Identifier.js';
 import IfStatement from './IfStatement.js';
-import Import from './Import.js';
-import ImportDeclaration from './ImportDeclaration.js';
-import ImportDefaultSpecifier from './ImportDefaultSpecifier.js';
-import ImportSpecifier from './ImportSpecifier.js';
 import JSXAttribute from './JSXAttribute.js';
 import JSXClosingElement from './JSXClosingElement.js';
 import JSXClosingFragment from './JSXClosingFragment.js';
@@ -20,11 +16,6 @@ import LoopStatement from './shared/LoopStatement.js';
 import MemberExpression from './MemberExpression.js';
 import ObjectExpression from './ObjectExpression.js';
 import Property from './Property.js';
-import ReturnStatement from './ReturnStatement.js';
-import Super from './Super.js';
-import TaggedTemplateExpression from './TaggedTemplateExpression.js';
-import TemplateElement from './TemplateElement.js';
-import TemplateLiteral from './TemplateLiteral.js';
 import ThisExpression from './ThisExpression.js';
 import UpdateExpression from './UpdateExpression.js';
 import VariableDeclaration from './VariableDeclaration.js';
@@ -35,10 +26,6 @@ export default {
 	FunctionExpression,
 	Identifier,
 	IfStatement,
-	Import,
-	ImportDeclaration,
-	ImportDefaultSpecifier,
-	ImportSpecifier,
 	JSXAttribute,
 	JSXClosingElement,
 	JSXClosingFragment,
@@ -52,11 +39,6 @@ export default {
 	MemberExpression,
 	ObjectExpression,
 	Property,
-	ReturnStatement,
-	Super,
-	TaggedTemplateExpression,
-	TemplateElement,
-	TemplateLiteral,
 	ThisExpression,
 	UpdateExpression,
 	VariableDeclaration,
